d5be441764b3cb43c2b8c59af745af892ed37e91
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-07-24 17:21:00 +0300

    feat(main): implement main program for word count and frequency

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
-#include<stdio.h>
+#include "../lib/input_handling.h"
+#include "../lib/word_count.h"
+#include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  printf("Hello, World!\n");
-  return 0;
+    printf("Hello, World!\n");
+
+    ReadBuffer *input_buffer = NULL;
+
+    if (argc > 1) {
+        input_buffer = read_from_file(argv[1]);
+    } else {
+        input_buffer = read_from_stdin();
+    }
+
+    // word counts
+    printf("input: %s\n----\n", input_buffer->data);
+    printf("count words: %d\n", count_words(input_buffer->data));
+
+    // word frequency count
+    WCNode *wc_list = count_word_frequencies(input_buffer->data, NULL);
+    print_list(wc_list);
+    printf("\n");
+
+    // free input_buffer
+    if (input_buffer)
+        free_read_buffer(input_buffer);
+    // free wc_list
+    if (wc_list)
+        free_list(wc_list);
+
+    return 0;
 }
