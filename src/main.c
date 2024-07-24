c0d408614d6755ec3f8761477fca196cb4d89e7a
m7moudGadallah <mahmoudgadallah876@gmail.com>
2024-07-24 18:11:22 +0300

    feat(main): add sort command option

--- src/main.c
+++ src/main.c
@@ -5,20 +5,32 @@
 #include <string.h>
 
 void print_usage(const char *);
+int compare_wc_nodes(WCNode *, WCNode *);
 
 int main(int argc, char *argv[]) {
     bool count_flag = false;
     bool freq_flag = false;
+    bool sort_flag = false;
     char *file_path = NULL;
 
     for (int i = 1; i < argc; ++i) {
-        if (strcmp(argv[i], "-c") == 0) {
-            count_flag = true;
-        } else if (strcmp(argv[i], "-f") == 0) {
-            freq_flag = true;
-        } else if (strcmp(argv[i], "-cf") == 0) {
-            count_flag = 1;
-            freq_flag = 1;
+        if (argv[i][0] == '-') {
+            for (int j = 1; argv[i][j] != '\0'; ++j) {
+                switch (argv[i][j]) {
+                case 'c':
+                    count_flag = true;
+                    break;
+                case 'f':
+                    freq_flag = true;
+                    break;
+                case 's':
+                    sort_flag = true;
+                    break;
+                default:
+                    print_usage(argv[0]);
+                    return 1;
+                }
+            }
         } else {
             file_path = argv[i];
         }
@@ -41,6 +53,11 @@ int main(int argc, char *argv[]) {
     // word frequency count
     if (freq_flag) {
         WCNode *wc_list = count_word_frequencies(input_buffer->data, NULL);
+
+        if (sort_flag) {
+            sort_wc_list(&wc_list, compare_wc_nodes);
+        }
+
         print_list(wc_list);
         printf("\n");
 
@@ -60,5 +77,13 @@ void print_usage(const char *prog_name) {
     printf("Options:\n");
     printf("  -c   Count words\n");
     printf("  -f   Count word frequencies\n");
+    printf("  -s   Sort word frequencies\n");
     printf("  -cf  Count words and word frequencies (default)\n");
+}
+
+int compare_wc_nodes(WCNode *node1, WCNode *node2) {
+    int comp = node2->count - node1->count;
+    return (comp) ? comp
+                  : strcmp(node1->word,
+                           node2->word); // Sorting in descending order of count
 }
